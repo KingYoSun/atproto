e76d71051be86e1044d1b3199eeda0963383417e
KingYoSun <nocking257838@gmail.com>
2023-10-23 13:59:26 +0900

    show doc when no id document

--- packages/identity/src/did/atproto-data.ts
+++ packages/identity/src/did/atproto-data.ts
@@ -4,7 +4,7 @@ import { DidDocument, AtprotoData } from '../types'
 export const getDid = (doc: DidDocument): string => {
   const id = doc.id
   if (typeof id !== 'string') {
-    throw new Error('No `id` on document')
+    throw new Error(`No 'id' on document: ${JSON.stringify(doc)}`)
   }
   return id
 }
